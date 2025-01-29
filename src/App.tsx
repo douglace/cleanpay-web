47e8095c3cc660ee61fb7db31c914f9da8b4f2f8
Dilios Team Dev - DK <31247228+douglace@users.noreply.github.com>
2025-01-29 17:39:33 +0100

    install taillwind

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Layout from "./components/layout/layout.tsx";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h2 className={"bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3"}>CleanPay Web</h2>
+      <Layout/>
     </>
   )
 }
